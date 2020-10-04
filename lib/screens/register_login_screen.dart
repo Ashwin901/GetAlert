9de0d2e1b9319c1f8d8a2ec846fb62ffdbd5b338
Ashwin p <ashwinprasanna9@gmail.com>
2020-10-04 13:07:36 +0530

    firebase auth error handling

--- lib/screens/register_login_screen.dart
+++ lib/screens/register_login_screen.dart
@@ -15,7 +15,7 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
   TextEditingController passwordController;
   String email;
   String password;
-  FirebaseAuth auth ;
+  FirebaseAuth auth;
   var title;
   @override
   void initState() {
@@ -26,6 +26,7 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
     title = widget.title;
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,11 +34,7 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('GetAlert',
-            style: textStyle.copyWith(
-              fontSize: 30
-            )
-          ),
+          Text('GetAlert', style: textStyle.copyWith(fontSize: 30)),
           SizedBox(
             height: 6,
           ),
@@ -46,18 +43,17 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
             child: TextField(
               style: textStyle,
               keyboardType: TextInputType.emailAddress,
-              onChanged: (value){
+              onChanged: (value) {
                 setState(() {
                   email = value;
                 });
-
               },
               controller: emailController,
-              textAlign: TextAlign.center ,
+              textAlign: TextAlign.center,
               showCursor: true,
               cursorColor: Colors.black,
               decoration: InputDecoration(
-                filled: true,
+                  filled: true,
                   fillColor: Colors.white,
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black87),
@@ -67,10 +63,7 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
                   hintStyle: textStyle,
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(color: Colors.white
-                      )
-                  )
-              ),
+                      borderSide: BorderSide(color: Colors.white))),
             ),
           ),
           SizedBox(
@@ -80,11 +73,11 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
             padding: EdgeInsets.all(6),
             child: TextField(
               style: textStyle,
-              onChanged: (value){
+              onChanged: (value) {
                 password = value;
               },
               controller: passwordController,
-              textAlign: TextAlign.center ,
+              textAlign: TextAlign.center,
               showCursor: true,
               cursorColor: Colors.black,
               obscureText: true,
@@ -95,8 +88,7 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
                 hintText: "enter password",
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: Colors.white)
-                ),
+                    borderSide: BorderSide(color: Colors.white)),
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.black87),
                   borderRadius: BorderRadius.circular(12),
@@ -104,40 +96,39 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
               ),
             ),
           ),
-
           RaisedButton(
             padding: EdgeInsets.all(15),
             shape: RoundedRectangleBorder(
               borderRadius: new BorderRadius.circular(30.0),
             ),
             child: Text(title,
-              style: textStyle.copyWith(
-                  color: Color(0xffffc93c)
-              )
-            ),
-            onPressed: () async{
+                style: textStyle.copyWith(color: Color(0xffffc93c))),
+            onPressed: () async {
               var user;
-              if(title == "register"){
-                 user = await auth.createUserWithEmailAndPassword(email: email, password: password).catchError((e){
-                   print(e.code);
-                   handleErrors(e.code, context);
-                 });
-              }else{
-                user = await auth.signInWithEmailAndPassword(email: email, password: password).catchError((e){
+              if (title == "register") {
+                user = await auth
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password)
+                    .catchError((e) {
                   print(e.code);
-                  handleErrors(e.code,context);
+                  handleErrors(e.code, context);
+                });
+              } else {
+                user = await auth
+                    .signInWithEmailAndPassword(
+                        email: email, password: password)
+                    .catchError((e) {
+                  print(e.code);
+                  handleErrors(e.code, context);
                 });
               }
               emailController.clear();
               passwordController.clear();
-              if(user != null){
-                   Navigator.push(context, MaterialPageRoute(
-                     builder: (context){
-                       return MainScreen();
-                     }
-                   ));
+              if (user != null) {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return MainScreen();
+                }));
               }
-
             },
             color: title == 'register' ? Colors.black : Colors.white,
           )
@@ -147,43 +138,57 @@ class _RegisterOrLoginScreenState extends State<RegisterOrLoginScreen> {
   }
 }
 
-void handleErrors(String errorCode, BuildContext context){
+void handleErrors(String errorCode, BuildContext context) {
   var errorMessage;
-  switch(errorCode){
-    case 'user-not-found': errorMessage = "Invalid username or password";
-    break;
-    case 'email-already-exists' : errorMessage = "User already exits";
-    break;
-    case 'invalid-email' : errorMessage ="Please enter a valid email address";
-    break;
-    case 'invalid-password' : errorMessage ="Invalid password";
-    break;
-    case 'weak-password' : errorMessage = 'Password should have more than six characters';
-    break;
-//    case 'wrong-password' : errorMessage = 'Invalid password';
-//    break;
-    default : errorMessage = '';
+  switch (errorCode) {
+    case 'user-not-found':
+      errorMessage = "Invalid username or password";
+      break;
+    case 'email-already-exists':
+      errorMessage = "User already exits";
+      break;
+    case 'invalid-email':
+      errorMessage = "Please enter a valid email address";
+      break;
+    case 'invalid-password':
+      errorMessage = "Invalid password";
+      break;
+    case 'weak-password':
+      errorMessage = 'Password should have more than six characters';
+      break;
+    case 'wrong-password':
+      errorMessage = 'Invalid password';
+      break;
+    default:
+      errorMessage = '';
   }
-  if(errorMessage.length >1){
-    showDialog(context: (context),
-    builder:(context){
-      return AlertDialog(
-        backgroundColor: Color(0xffffc93c),
-        title: Text('Something went wrong',style: textStyle,),
-        content: Text(errorMessage,style: textStyle,),
-        actions: [
-          RaisedButton(
-            child: Text('ok',style: textStyle.copyWith(
-              color: Color(0xffffc93c)
-            ),),
-            color: Colors.black,
-            onPressed: (){
-              Navigator.pop(context);
-            },
-          )
-        ],
-      );
-    }
-    );
+  if (errorMessage.length > 1) {
+    showDialog(
+        context: (context),
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Color(0xffffc93c),
+            title: Text(
+              'Something went wrong',
+              style: textStyle,
+            ),
+            content: Text(
+              errorMessage,
+              style: textStyle,
+            ),
+            actions: [
+              RaisedButton(
+                child: Text(
+                  'ok',
+                  style: textStyle.copyWith(color: Color(0xffffc93c)),
+                ),
+                color: Colors.black,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
   }
 }
